4a26cc0d44a9a14f8a4cade81e18bd339f4132da
Christophe Jacquet <ChristopheJacquet@users.noreply.github.com>
2012-12-08 15:39:52 +0100

    Corr. squelette WSGI

--- web_scandeur.py
+++ web_scandeur.py
@@ -3,9 +3,8 @@
 
 from cgi import escape
 import sys, os
-from flup.server.fcgi import WSGIServer
 
-def app(environ, start_response):
+def application(environ, start_response):
     start_response('200 OK', [('Content-Type', 'text/html')])
 
     yield '<h1>FastCGI Environment</h1>'
@@ -14,4 +13,3 @@ def app(environ, start_response):
          yield '<tr><th>%s</th><td>%s</td></tr>' % (escape(k), escape(v))
     yield '</table>'
 
-WSGIServer(app).run()
