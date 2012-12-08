6410427135c06c266553845f0cacfb00c1ef6b35
Christophe Jacquet <ChristopheJacquet@users.noreply.github.com>
2012-12-08 16:24:42 +0100

    Traitement post

--- web_scandeur.py
+++ web_scandeur.py
@@ -1,15 +1,26 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 
-from cgi import escape
+import cgi
 import sys, os
 
-def application(environ, start_response):
+def application(env, start_response):
     start_response('200 OK', [('Content-Type', 'text/html')])
 
-    yield '<h1>FastCGI Environment</h1>'
-    yield '<table>'
-    for k, v in sorted(environ.items()):
-         yield '<tr><th>%s</th><td>%s</td></tr>' % (escape(k), escape(v))
-    yield '</table>'
+    post_env = env.copy()
+    post_env['QUERY_STRING'] = ''
+    post = cgi.FieldStorage(
+        fp=env['wsgi.input'],
+        environ=post_env,
+        keep_blank_values=True
+    )
+    
+    yield repr(post)
+
+
+#     yield '<h1>FastCGI Environment</h1>'
+#     yield '<table>'
+#     for k, v in sorted(environ.items()):
+#          yield '<tr><th>%s</th><td>%s</td></tr>' % (escape(k), escape(v))
+#     yield '</table>'
 
